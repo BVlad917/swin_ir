6a845fe9512fc5f35734e3cec60c561a5016a450
bogdan.vlad <bogdan.vlad@grabtaxi.com>
2023-04-15 17:28:35 +0300

    add residual swin transformer block from swin IR

---
+++ swin_ir_components/residual_swin_transformer_block.py
@@ -0,0 +1,38 @@
+from torch import nn
+
+from swin_v1_components.swin_v1_transformer_block import SwinTransformerBlock
+
+
+class ResidualSwinTransformerBlock(nn.Module):
+    def __init__(self, num_layers, input_dim, heads, head_dim, window_size, drop_path_probs, attn_drop_prob,
+                 proj_drop_prob):
+        super().__init__()
+        # checks
+        assert num_layers % 2 == 0, "Stage layers need to be divisible by 2 for regular and shifted blocks"
+        if isinstance(drop_path_probs, list):
+            assert len(drop_path_probs) == num_layers, "Must give exactly one DropPath rate to each layer"
+
+        dpp = drop_path_probs  # abbreviation, just to have clean code below
+        self.swin_transformer_blocks = nn.Sequential(
+            *[SwinTransformerBlock(input_dim=input_dim,
+                                   heads=heads,
+                                   head_dim=head_dim,
+                                   window_size=window_size,
+                                   shifted=(i % 2 == 1),
+                                   mlp_dim=input_dim * 4,
+                                   drop_path_prob=dpp[i] if isinstance(dpp, list) else dpp,
+                                   attn_drop_prob=attn_drop_prob,
+                                   proj_drop_prob=proj_drop_prob)
+              for i in range(num_layers)]
+        )
+        self.conv = nn.Conv2d(in_channels=input_dim,
+                              out_channels=input_dim,
+                              kernel_size=3,
+                              stride=1,
+                              padding=1)
+
+    def forward(self, x):
+        # input shape: (B, C, H, W)
+        x = self.swin_transformer_blocks(x)  # (B, C, H, W)
+        x = self.conv(x)  # (B, C, H, W)
+        return x
--- swin_ir_components/shallow_feature_extractor.py
+++ swin_ir_components/shallow_feature_extractor.py
@@ -2,16 +2,16 @@ from torch import nn
 
 
 class ShallowFeatureExtractor(nn.Module):
-    def __init__(self, in_channels, embed_dim, kernel_size):
+    def __init__(self, in_channels, embed_dim):
         super().__init__()
         # Conv layer with "same" padding
         self.conv = nn.Conv2d(in_channels=in_channels,
                               out_channels=embed_dim,
-                              kernel_size=kernel_size,
+                              kernel_size=3,
                               stride=1,
                               padding=1)
 
     def forward(self, x):
-        # input shape: (B, C_in, H, W)
-        x = self.conv(x)  # (B, C_out, H, W)
+        # input shape: (B, C, H, W)
+        x = self.conv(x)  # (B, EMBED_DIM, H, W)
         return x
